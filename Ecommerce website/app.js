a23e41039489ecb366e813837736225fa1ba76ea
zaiba khan <129111102+zaibakhan66@users.noreply.github.com>
2023-08-29 16:28:22 +0500

    Add files via upload

---
+++ Ecommerce website/app.js
@@ -0,0 +1,20 @@
+let card = document.querySelector(".trend");
+let card2 = document.getElementById("trendSec");
+
+console.log(card);
+let blog = document.querySelector(".trends");
+let mainPage = document.querySelector(".main");
+
+function shops(){
+
+    mainPage.style.display="none";
+    blog.style.display="none";
+}
+// function blogs(){
+//     mainPage.style.display="none";
+//     card.style.display="none";
+//     card2.style.display="none";
+//     blog.style.display="block";
+
+
+// }
